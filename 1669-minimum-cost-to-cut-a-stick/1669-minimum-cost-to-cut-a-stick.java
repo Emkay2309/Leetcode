fdf4383f06fd6831ace10ba6a651a43b33afa6b9
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2024-08-22 18:42:54 +0530

    Time: 30 ms (16.88%), Space: 44.2 MB (10.25%) - LeetHub

---
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int minCost(int n, int[] cuts) {
+
+        int m = cuts.length;
+        if (m == 1)
+            return n;
+
+        ArrayList<Integer> stick = new ArrayList<>();
+        stick.add(0);
+        Arrays.sort(cuts);
+        for (int a : cuts) {
+            stick.add(a);
+        }
+        stick.add(n);
+
+        int[][] dp = new int[102][102];
+        for (int[] a : dp) {
+            Arrays.fill(a, -1);
+        }
+
+        return dfs(0, stick.size() - 1, stick, dp);
+    }
+
+    public int dfs(int left, int right, ArrayList<Integer> stick, int[][] dp) {
+        if (right - left < 2)
+            return 0;
+
+        if (dp[left][right] != -1)
+            return dp[left][right];
+
+        int ans = Integer.MAX_VALUE;
+
+        for (int cut = left + 1; cut <= right - 1; cut++) {
+
+            int curr = (stick.get(right) - stick.get(left)) + dfs(left, cut, stick, dp)
+                    + dfs(cut, right, stick, dp);
+
+            int total = curr;
+
+            ans = Math.min(ans, total);
+        }
+        return dp[left][right] = ans;
+    }
+}
