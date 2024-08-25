2922dd7cc82180688bbabd31df1d6e832f3d0be7
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2024-08-25 18:52:34 +0530

    Time: 99 ms (37.87%), Space: 43.9 MB (21.33%) - LeetHub

--- 0839-similar-string-groups/0839-similar-string-groups.java
+++ 0839-similar-string-groups/0839-similar-string-groups.java
@@ -8,24 +8,31 @@ class Solution {
             adj.add(new ArrayList<>());
         }
 
+        DSU dsu = new DSU(n);
+        int group = n;
         for(int i=0; i<n ; i++) {
             for(int j=i+1 ; j<n ; j++) {
-                if(checkSimilar(strs[i] , strs[j])) {
+                if(checkSimilar(strs[i] , strs[j]) && dsu.find(i) != dsu.find(j)) {
+
+                    dsu.union(i,j);
+                    group--;
                     adj.get(i).add(j);
                     adj.get(j).add(i);
                 }
             }
         }
 
-        boolean [] vis = new boolean [n];
-        int count = 0;
-        for(int i=0 ;i<n ; i++) {
-            if(!vis[i]) {
-                dfs(i , vis ,adj);
-                count++;
-            }
-        }
-        return count;
+        return group;
+
+        // boolean [] vis = new boolean [n];
+        // int count = 0;
+        // for(int i=0 ;i<n ; i++) {
+        //     if(!vis[i]) {
+        //         dfs(i , vis ,adj);
+        //         count++;
+        //     }
+        // }
+        // return count;
     }
 
     public boolean checkSimilar(String s1 , String s2) {
@@ -48,6 +55,44 @@ class Solution {
             if(!vis[neigh]) {
                 dfs(neigh , vis , adj);
             }
+        }
+    }
+
+}
+
+class DSU {
+    int [] rank ;
+    int [] par;
+    DSU(int n) {
+        this.rank = new int [n];
+        this.par = new int [n];
+
+        for(int i=0; i<n ; i++) {
+            rank[i] = 1;
+            par[i] = i;
+        }
+    }
+
+    public int find(int u) {
+        if(u == par[u]) return u;
+        return par[u] = find(par[u]);
+    }
+
+    public void union(int u  ,int v) {
+        int up = find(u);
+        int vp = find(v);
+
+        if(up == vp) return;
+
+        if(rank[up] > rank[vp]) {
+            par[vp] = up; 
+        }
+        else if(rank[up] < rank[vp]) {
+            par[up] = vp;
+        }
+        else {
+            par[up] = vp;
+            vp++;
         }
     }
 }
