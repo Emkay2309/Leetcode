1dc7dbf758479aaf7d56bbe4defa28e26be66b09
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2024-08-30 19:38:24 +0530

    Time: 43 ms (23.57%), Space: 44.8 MB (74.56%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int removeStones(int[][] stones) {
+        int n = stones.length;
+        boolean [] vis = new boolean [n];
+        int group = 0;
+
+        for(int i=0; i<n ; i++) {
+            if(!vis[i]) {
+                dfs(i,vis,stones);
+                group++;
+            }
+        }
+        return n - group;
+    }
+
+    public void dfs(int index , boolean [] vis , int [][] stones) {
+        vis[index] = true; // vis[0] = true
+
+        int r = stones[index][0]; // 0
+        int c = stones[index][1]; // 1
+
+
+        //Iterate on the complete stones matrix to check any row or col same
+        for(int i=0; i<stones.length ; i++) {
+            if((!vis[i] ) && (stones[i][0] == r || stones[i][1] == c)) {
+                dfs(i , vis , stones);
+            }
+        }
+    }
+}
