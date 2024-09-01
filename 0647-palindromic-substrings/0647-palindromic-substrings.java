b66b726264c43eab2fe88d0802a9ee08e69def76
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2024-09-01 17:36:07 +0530

    Time: 35 ms (25.44%), Space: 58.2 MB (5.1%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -3,20 +3,27 @@ class Solution {
         int n = s.length();
         int ans = 0;
 
+        Boolean [][] dp = new Boolean [n][n];
+        for(Boolean [] a : dp) {
+            Arrays.fill(a , null);
+        }
+
         for(int i=0 ; i<n ; i++) {
             for(int j=i ; j<n ; j++) {
-                if(check( i , j , s)) ans++;
+                if(check( i , j , s , dp)) ans++;
             }
         }
         return ans;
     }
 
-    public boolean check(int i, int j , String s) {
+    public boolean check(int i, int j , String s , Boolean [][] dp) {
         if(i > j) return true;
 
+        if(dp[i][j] != null) return dp[i][j];
+
         if(s.charAt(i) == s.charAt(j)) {
-            return check(i+1 , j-1 , s);
+            return dp[i][j] = check(i+1 , j-1 , s , dp);
         }
-        return false;
+        return  dp[i][j] = false;
     }
 }
