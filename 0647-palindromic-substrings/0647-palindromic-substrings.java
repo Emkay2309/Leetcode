4faf67eb49f603a571a559bcba3d3360e1ef92a6
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2024-09-01 16:48:07 +0530

    Time: 934 ms (5.35%), Space: 41.4 MB (65.54%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,20 +1,22 @@
 class Solution {
-
-    public boolean isPalindrome(String s, int left, int right) {
-        while(left < right) {
-            if(s.charAt(left++) != s.charAt(right--)) return false;
-        } 
-        return true;
-    }
-    
     public int countSubstrings(String s) {
-        int ans = 0;
         int n = s.length();
-        for(int i=0;i<n;i++) {
-            for(int j=i;j<n;j++) {
-                if(isPalindrome(s, i, j)) ans++;
+        int ans = 0;
+
+        for(int i=0 ; i<n ; i++) {
+            for(int j=i ; j<n ; j++) {
+                if(check( i , j , s)) ans++;
             }
         }
         return ans;
+    }
+
+    public boolean check(int i, int j , String s) {
+        if(i > j) return true;
+
+        if(s.charAt(i) == s.charAt(j)) {
+            return check(i+1 , j-1 , s);
+        }
+        return false;
     }
 }
