13fff6748654ac304cc3f4e593c0a32c0a14fe7a
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2024-09-16 14:33:19 +0530

    Time: 3 ms (49.04%), Space: 45.7 MB (31.88%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,32 +1,13 @@
 class Solution {
-    // public boolean canJump(int[] nums) {
-    //     int sum = 0;
-    //     for(int i=0;i<nums.length;i++) {
-    //         sum = Math.max(sum-1, nums[i]);
-    //         if(sum==0 && i<nums.length-1) return false;
-    //     }
-    //     return true;
-    // }
-    public boolean fun(int i, int [] A, int [] dp){
-        if(i>= A.length) return false;
-        if(i== A.length -1) return true;
-        if(dp[i]!=-1){
-            if(dp[i]==1) return true;
-            return false;
-        }
-       boolean res= false;
-       for(int j=1;j<= A[i];j++){
-           res= (res|| fun(i+j, A,dp));
-       }
-       if(res==false) dp[i]=0;
-       if(res== true) dp[i]=1;
-       return res;
-    }
     public boolean canJump(int[] nums) {
-       int dp[] = new int[nums.length+1];
-       for(int i=0; i<=nums.length; i++){
-           dp[i]=-1;
-       }
-       return fun(0, nums,dp); 
+        int n = nums.length;
+        int maxReachable = 0;
+
+        for(int i=0 ; i<n ; i++) {
+            if(i > maxReachable) return false;
+            int max = i+nums[i];
+            maxReachable = Math.max(maxReachable , max);
+        }
+        return true;
     }
 }
