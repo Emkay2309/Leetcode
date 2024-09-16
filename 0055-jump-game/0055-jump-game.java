d28ef812ab003194f271fc4422cba522478be0bd
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2024-09-16 14:22:58 +0530

    Time: 2757 ms (5.02%), Space: 43.6 MB (99.87%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,22 +1,32 @@
 class Solution {
-    public boolean canJump(int[] nums) {
-        Boolean [] dp = new Boolean [nums.length];
-        Arrays.fill(dp , null);
-        return dfs(0 , nums , dp);
-    }
-    public boolean dfs(int i , int [] nums , Boolean [] dp) {
-        if(i == nums.length-1) return true;
-
-        if(i >= nums.length ) return false;
-
-        if(dp[i] != null) return dp[i];
-
-        for(int j=1 ; j<=nums[i] ; j++) {
-            boolean check = dfs(i+j , nums , dp);
-            if(check ) 
-                return dp[i] = true;
+    // public boolean canJump(int[] nums) {
+    //     int sum = 0;
+    //     for(int i=0;i<nums.length;i++) {
+    //         sum = Math.max(sum-1, nums[i]);
+    //         if(sum==0 && i<nums.length-1) return false;
+    //     }
+    //     return true;
+    // }
+    public boolean fun(int i, int [] A, int [] dp){
+        if(i>= A.length) return false;
+        if(i== A.length -1) return true;
+        if(dp[i]!=-1){
+            if(dp[i]==1) return true;
+            return false;
         }
-
-        return dp[i] = false;
+       boolean res= false;
+       for(int j=1;j<= A[i];j++){
+           res= (res|| fun(i+j, A,dp));
+       }
+       if(res==false) dp[i]=0;
+       if(res== true) dp[i]=1;
+       return res;
+    }
+    public boolean canJump(int[] nums) {
+       int dp[] = new int[nums.length+1];
+       for(int i=0; i<=nums.length; i++){
+           dp[i]=-1;
+       }
+       return fun(0, nums,dp); 
     }
 }
