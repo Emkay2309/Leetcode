9532d41391327193ba687ad3d80c87c3d3234f5a
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2024-09-18 10:19:21 +0530

    Time: 363 ms (6.61%), Space: 45.9 MB (22.34%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,28 +1,25 @@
 class Solution {
-    public boolean canJump(int [] nums) {
+    public boolean canJump(int[] nums) {
         int n = nums.length;
-        if(n == 1 && nums[0] == 0) return true;
         Boolean [] dp = new Boolean [n];
         Arrays.fill(dp , null);
-        if(nums[0] == 0) return false; 
-        return dfs( 0 , nums , dp);
+        return dfs(0 , n, nums , dp);
     }
 
-    public boolean dfs(int i , int [] nums , Boolean [] dp) {
-        if(i == nums.length-1) return true;
+    public boolean dfs(int i , int n , int [] nums , Boolean [] dp) {
+        if(i == n-1) return true;
+        if(i >= n) return false;
 
-        if(i >= nums.length) return false;
+        if(dp[i] != null) return dp[i];
 
-        // recursive call 
-        int times = nums[i]; //2
+        int size = nums[i];
 
-        for(int j=1 ; j<=times ; j++) {
-            Boolean check =dfs( i + j , nums , dp);
-            if(check) {
-                return dp[i] = true;
-            }    
+        for(int j=1 ; j<=size ; j++) {
+            if(dfs(j+i , n , nums , dp)) {
+                return dp[i] =  true;
+            }
         }
+
         return dp[i] = false;
-        
     }
 }
