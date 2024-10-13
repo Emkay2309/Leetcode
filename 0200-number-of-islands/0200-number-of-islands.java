c92d12ed3714a3b5f4c66b17aff85503df71a14f
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2024-10-13 14:31:21 +0530

    Time: 2 ms (99.81%), Space: 49.6 MB (44.82%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -2,26 +2,28 @@ class Solution {
     public int numIslands(char[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        int count = 0;
-        for(int i=0; i<n ; i++ ) {
+
+        int ans = 0;
+
+        for(int i=0 ; i<n ; i++) {
             for(int j=0 ; j<m ; j++) {
                 if(grid[i][j] == '1') {
-                    dfs(i , j , n , m ,grid);
-                    count++;
+                    ans++;
+                    dfs(i , j , n , m , grid);
                 }
             }
         }
-        return count;
+        return ans;
     }
-    public void dfs(int i , int j , int n , int m , char [] [] grid) {
-        if(i<0 || i>=n || j<0 || j>=m || grid[i][j]=='0') {
-            return;
-        }
-        grid[i][j] = '0';
 
-        dfs(i+1,j,n,m,grid);
-        dfs(i-1,j,n,m,grid);
-        dfs(i ,j+1 , n , m ,grid);
-        dfs(i, j-1 , n , m , grid);
+    public void dfs(int i , int j , int n , int m , char [][] mat) {
+        if(i<0 || i>=n || j<0 || j>=m || mat[i][j] == '0') return;
+
+        mat[i][j] = '0';
+
+        dfs(i+1 , j , n , m , mat);
+        dfs(i-1 , j , n , m , mat);
+        dfs(i , j-1 , n , m , mat);
+        dfs( i , j+1 , n , m , mat);
     }
 }
