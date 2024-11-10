0de12842803651657d942d1a60bdf68ad177a512
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2024-11-10 14:11:06 +0530

    Time: 2 ms (98.59%), Space: 43.2 MB (92.97%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,25 +1,24 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int ans = 0;
         int n = s.length();
-        int [] freq = new int [128];
+        int ans = 0;
         int left = 0;
+        int [] freq = new int [128];
 
-        for(int right=0 ; right < n ; right++)  {
+        for(int right=0 ; right<n ; right++) {
             int add = s.charAt(right);
             freq[add]++;
 
-            if(freq[add] == 1)  {
-                int len = right - left + 1;
-                ans = Math.max( ans , len);
+            if(freq[add] == 1) {
+                ans = Math.max(ans , right-left+1);
             }
             else {
-                while(freq[add] > 1  && left < n)  {
+                while(freq[add] > 1 && left < n) {
                     int remove = s.charAt(left);
                     freq[remove]--;
                     left++;
                 }
-            }
+            }   
         }
         return ans;
     }
