a1ab752804f070e42f827c237920df7c76f0c08e
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2025-01-16 15:46:57 +0530

    Time: 0 ms (100%), Space: 42.2 MB (57.29%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -14,20 +14,62 @@
  * }
  */
 class Solution {
-    public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        if(root == null) return ans;
-        dfs(root , 1 , ans);
-        return ans;
-    }
 
-    public void dfs(TreeNode root , int level , List<Integer> ans) {
+    public void preOrder(TreeNode root , int level , List<Integer>ans) {
         if(root == null) return;
 
-        if(level > ans.size()) {
-            ans.add(root.val);
+        if(ans.size() < level) ans.add(root.val);
+
+        preOrder(root.right , level+1 , ans);
+        preOrder(root.left , level+1 , ans);
+    }
+    public List<Integer> rightSideView(TreeNode root) {
+        if(root == null) return new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
+        preOrder(root , 1 , ans);
+        return ans;
+
+        // List<List<Integer>> ans = levelOrder(root);
+
+        // List<Integer> ans1 = new ArrayList<>();
+
+        // for(int i=0 ; i<ans.size() ; i++) {
+        //     for(int j=0 ; j<ans.get(i).size() ; j++) {
+        //         if(j == ans.get(i).size()-1 ) {
+        //             ans1.add(ans.get(i).get(j));
+        //         }
+        //     }
+        // }
+
+        // return ans1;
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root == null) return ans;
+        
+        Queue<TreeNode> q = new LinkedList<>();
+
+        q.add(root);
+
+        while(!q.isEmpty()) {
+            int size = q.size();
+            ArrayList<Integer> temp = new ArrayList<>();
+
+            for(int i=0 ; i<size ; i++) {
+                TreeNode curr = q.poll();
+
+                if(curr.left != null) {
+                    q.add(curr.left);
+                }
+                if(curr.right != null) {
+                    q.add(curr.right);
+                }
+                temp.add(curr.val);
+            }
+            ans.add(new ArrayList<>(temp));
         }
-        dfs(root.right , level+1 , ans);
-        dfs(root.left  , level+1 , ans);
+
+        return ans;
     }
 }
