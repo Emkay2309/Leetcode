4e953de80eafcbae016cb62c0514aed5d4ca3781
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2025-03-20 23:32:19 +0530

    Time: 14 ms (17.09%), Space: 42.7 MB (36.26%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.java
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.java
@@ -1,34 +1,40 @@
 class Solution {
-    public int[][] directions = { { 1, 2 }, { 1, -2 }, { -1, 2 }, { -1, -2 }, { 2, 1 }, { 2, -1 }, { -2, 1 }, { -2, -1 } };
-
-    public double[][][] memo;
-
     public double knightProbability(int n, int k, int row, int column) {
-        memo = new double[n][n][k + 1];
-        for (double[][] layer : memo) {
-            for (double[] rowArr : layer) {
-                Arrays.fill(rowArr, -1.0);
+        // DP table where dp[step][r][c] stores probability of being on (r,c) after "step" moves
+        double[][][] dp = new double[k + 1][n][n];
+
+        // Base case: If no moves are left, the knight is definitely on the board
+        dp[0][row][column] = 1.0;
+
+        // All possible knight moves
+        int[][] directions = {{1,2}, {1,-2}, {-1,2}, {-1,-2}, {2,1}, {2,-1}, {-2,1}, {-2,-1}};
+
+        // Iterate over the number of moves from 1 to k
+        for (int step = 1; step <= k; step++) {
+            for (int r = 0; r < n; r++) {
+                for (int c = 0; c < n; c++) {
+                    // Transition from previous step
+                    for (int[] dir : directions) {
+                        int prevRow = r - dir[0];
+                        int prevCol = c - dir[1];
+
+                        // Check if the previous position was within bounds
+                        if (prevRow >= 0 && prevRow < n && prevCol >= 0 && prevCol < n) {
+                            dp[step][r][c] += dp[step - 1][prevRow][prevCol] / 8.0;
+                        }
+                    }
+                }
             }
         }
-        return solve(row, column, k, n);
-    }
 
-    public double solve(int r, int c, int k, int n) {
-        if (r < 0 || r >= n || c < 0 || c >= n) return 0.0;
-
-        if (k == 0) return 1.0;
-
-        if (memo[r][c][k] != -1.0)  return memo[r][c][k];
-
-        double ans = 0.0;
-
-        for (int[] dir : directions) {
-            int n_row = r + dir[0];
-            int n_col = c + dir[1];
-
-            ans += solve(n_row, n_col, k - 1, n) / 8.0;
+        // Sum up probabilities after k moves
+        double result = 0.0;
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < n; c++) {
+                result += dp[k][r][c];
+            }
         }
 
-        return memo[r][c][k] = ans;
+        return result;
     }
 }
