26ca97dd949f94b91423db9a3fda103f874a535f
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2025-03-20 23:33:02 +0530

    Time: 4 ms (97.12%), Space: 42.9 MB (28.27%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.java
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.java
@@ -1,55 +1,34 @@
 class Solution {
+    public int[][] directions = { { 1, 2 }, { 1, -2 }, { -1, 2 }, { -1, -2 }, { 2, 1 }, { 2, -1 }, { -2, 1 }, { -2, -1 } };
+
+    public double[][][] memo;
+
     public double knightProbability(int n, int k, int row, int column) {
-        // Two DP arrays to store current and previous step probabilities
-        double[][] prev = new double[n][n];
-        double[][] curr = new double[n][n];
-
-        // Base case: The knight starts at (row, column)
-        prev[row][column] = 1.0;
-
-        // All possible knight moves
-        int[][] directions = {{1,2}, {1,-2}, {-1,2}, {-1,-2}, {2,1}, {2,-1}, {-2,1}, {-2,-1}};
-
-        // Iterate over the number of moves
-        for (int step = 1; step <= k; step++) {
-            // Reset current step DP table
-            for (int r = 0; r < n; r++) {
-                for (int c = 0; c < n; c++) {
-                    curr[r][c] = 0.0;
-                }
-            }
-
-            // Fill the DP table for the current step
-            for (int r = 0; r < n; r++) {
-                for (int c = 0; c < n; c++) {
-                    if (prev[r][c] > 0) {  // Only process cells with probability > 0
-                        for (int[] dir : directions) {
-                            int newRow = r + dir[0];
-                            int newCol = c + dir[1];
-
-                            // If the new position is within bounds, add probability
-                            if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < n) {
-                                curr[newRow][newCol] += prev[r][c] / 8.0;
-                            }
-                        }
-                    }
-                }
-            }
-
-            // Swap `prev` and `curr` to save space (rolling array technique)
-            double[][] temp = prev;
-            prev = curr;
-            curr = temp;
-        }
-
-        // Sum up probabilities from the last valid `prev` array
-        double result = 0.0;
-        for (int r = 0; r < n; r++) {
-            for (int c = 0; c < n; c++) {
-                result += prev[r][c];
+        memo = new double[n][n][k + 1];
+        for (double[][] layer : memo) {
+            for (double[] rowArr : layer) {
+                Arrays.fill(rowArr, -1.0);
             }
         }
+        return solve(row, column, k, n);
+    }
 
-        return result;
+    public double solve(int r, int c, int k, int n) {
+        if (r < 0 || r >= n || c < 0 || c >= n) return 0.0;
+
+        if (k == 0) return 1.0;
+
+        if (memo[r][c][k] != -1.0)  return memo[r][c][k];
+
+        double ans = 0.0;
+
+        for (int[] dir : directions) {
+            int n_row = r + dir[0];
+            int n_col = c + dir[1];
+
+            ans += solve(n_row, n_col, k - 1, n) / 8.0;
+        }
+
+        return memo[r][c][k] = ans;
     }
 }
