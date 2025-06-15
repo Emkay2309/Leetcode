dd84242474fc17ff8388e4a096636e4cc71bb4df
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2025-06-15 11:38:54 +0530

    Time: 0 ms (100%), Space: 40.5 MB (82.32%) - LeetHub

---
+++ 2566-maximum-difference-by-remapping-a-digit/2566-maximum-difference-by-remapping-a-digit.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int minMaxDifference(int num) {
+        int firstNonNine = -1, firstDigit = -1;
+        int remaining = num;
+        while (remaining > 0) {
+            int digit = remaining % 10;
+            if (digit != 9) {
+                firstNonNine = digit;
+            }
+            firstDigit = digit;
+            remaining /= 10;
+        }
+        
+        remaining = num;
+        int min = 0, max = 0;
+        int multiplier = 1;
+        while (remaining > 0) {
+            int digit = remaining % 10;
+            
+            int minDigit = digit;
+            int maxDigit = digit;
+            
+            if (digit == firstDigit) {
+                minDigit = 0;
+            }
+
+            if (digit == firstNonNine) {
+                maxDigit = 9;
+            }
+
+            min += multiplier * minDigit;
+            max += multiplier * maxDigit;
+
+            multiplier *= 10;    
+            remaining /= 10;
+        }
+
+        return max - min;
+    }
+}
