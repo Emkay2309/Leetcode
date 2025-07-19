71c37ac5787c97c3f6482aeb48826b1f1b7e0365
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2025-07-19 17:08:04 +0530

    Time: 3 ms (99.69%), Space: 68.9 MB (21.23%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,45 +10,26 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        
-        if(head.next == null) return true;
+        ListNode slow = head , fast = head , prev = null;
 
-        if(head.next.next == null) {
-            return head.val == head.next.val;
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+
+            ListNode temp = slow.next;
+            slow.next = prev;  // 1 -> 0
+            prev = slow;
+            slow = temp;
         }
 
-        ListNode mid = findMid(head);
-        ListNode l2 = reverse(mid.next);
-        mid.next = null;
-        ListNode l1 = head;
+        if(fast != null) {
+            slow = slow.next;
+        }
 
-        while(l1 != null && l2 != null) {
-            if(l1.val != l2.val) return false;
-
-            l1 = l1.next;
-            l2 = l2.next;
+        while(prev != null && slow != null) {
+            if(prev.val != slow.val) return false;
+            prev = prev.next;
+            slow = slow.next;
         }
         return true;
-    }
-
-    public ListNode findMid(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-
-        while( fast.next != null && fast.next.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        return slow;
-    }
-
-    public ListNode reverse(ListNode head) {
-        if(head == null || head.next == null) return head;
-        
-        ListNode temp = reverse(head.next);
-        head.next.next = head;
-        head.next = null;
-
-        return temp;
     }
 }
