89937ad61521925c33b65ff274313a3ccd34b7dd
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2025-07-20 05:17:46 +0530

    Time: 2 ms (16.37%), Space: 45.1 MB (48.21%) - LeetHub

--- 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.java
+++ 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.java
@@ -27,34 +27,22 @@ class Solution {
     public boolean isSubPath(ListNode head, TreeNode root) {
         if(root == null) return false;
 
-        //check if the current root has the exact linkedlist
-        //if not check for left and right children
-        boolean checkSame = check(head , root) ;
-        //if(checkSame) return true;
+        boolean check = checkList(head , root);
 
-        //check for children --- do they have the Linked List
         boolean left = isSubPath(head , root.left);
         boolean right = isSubPath(head , root.right);
 
-        //if any of them has required LinkedList , so use or 
-        return checkSame || left || right;
+        return left || right || check;
     }
 
-    public boolean check(ListNode head , TreeNode root) {
-        //if the head reached null means it traversed the complete linkedlist
+    public boolean checkList(ListNode head , TreeNode root) {
         if(head == null) return true;
-
-        //if the root completly traversed , then no LL found so false
         if(root == null) return false;
-
-        //check the values at both root and head same or not
         if(head.val != root.val) return false;
 
-        //call the next and left right child for iterating
-        boolean left =  check(head.next , root.left);
-        boolean right = check(head.next , root.right);
+        boolean left = checkList(head.next , root.left);
+        boolean right = checkList(head.next , root.right);
 
-        
-        return left || right;
+        return left||right;
     }
 }
