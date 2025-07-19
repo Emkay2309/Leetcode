981679f0ed55d66fd7866f18e73af8ca30e566b8
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2025-07-20 02:47:42 +0530

    Time: 4 ms (81.06%), Space: 47.4 MB (12.81%) - LeetHub

--- 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.java
+++ 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.java
@@ -10,49 +10,23 @@
  */
 class Solution {
     public ListNode doubleIt(ListNode head) {
-        if(head == null) return null;
+        int lastCarry = doubleUtil(head);
 
-        ListNode reversed = reverse(head);
-        ListNode added = add(reversed, reversed);
-        return reverse(added);
-    }
-
-    public ListNode add(ListNode l1 , ListNode l2)  {
-        ListNode ansHead = new ListNode(-1);
-        ListNode ans = ansHead;
-        int carry = 0;
-
-        while(l1 != null || l2 != null || carry>0) {
-            int val1 = (l1 != null) ? l1.val : 0;
-            int val2 = (l2 != null) ? l2.val : 0;
-
-            int sum = val1 + val2 + carry;
-
-            int add = sum%10;
-            carry = sum/10;
-
-            ListNode newNode = new ListNode(add);
-
-            ans.next = newNode;
-            ans = ans.next;
-
-            if(l1 != null) {
-                l1 = l1.next;
-            }
-            if(l2 != null) {
-                l2 = l2.next;
-            }
+        if(lastCarry > 0) {
+            ListNode carryNode = new ListNode(lastCarry);
+            carryNode.next = head;
+            return carryNode;
         }
-        return ansHead.next;
+        return head;
     }
 
-    public ListNode reverse(ListNode head) {
-        if(head == null || head.next == null) return head;
+    public int doubleUtil(ListNode head)  {
+        if(head == null) return 0;
 
-        ListNode last = reverse(head.next);
-        head.next.next = head;
-        head.next = null;
+        int carry = doubleUtil(head.next);
+        int value = (head.val * 2) + carry;
+        head.val = value%10;
 
-        return last;
+        return value/10;
     }
 }
