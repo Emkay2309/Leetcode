dce7f63f0df74fd3c3f4c8986c3485abcf4f31ac
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2025-07-27 00:31:56 +0530

    Time: 0 ms (100%), Space: 41.1 MB (77.47%) - LeetHub

--- 0814-binary-tree-pruning/0814-binary-tree-pruning.java
+++ 0814-binary-tree-pruning/0814-binary-tree-pruning.java
@@ -17,12 +17,29 @@ class Solution {
     public TreeNode pruneTree(TreeNode root) {
         if(root == null)  return null;
 
-        root.left = pruneTree(root.left);
-        root.right = pruneTree(root.right);
+        if(!is1Present(root.left)) {
+            root.left = null;
+        }
+        if(!is1Present(root.right)) {
+            root.right = null;
+        }
+        pruneTree(root.left);
+        pruneTree(root.right);
 
         if(root.val == 0 && root.left==null && root.right==null) {
             return null;
         }
         return root;
+    }
+
+    public boolean is1Present(TreeNode root) {
+        if(root == null) return false;
+
+        if(root.val == 1) return true;
+
+        boolean left = is1Present(root.left);
+        boolean right = is1Present(root.right);
+
+        return left || right;
     }
 }
