3c7501cac7622eedbdcf492740e51980f9f9a729
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2025-07-27 21:59:24 +0530

    Time: 0 ms (100%), Space: 41 MB (84.18%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,15 +15,31 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        
-        if(p==null && q==null) {
-            return true;
+        if(p==null && q==null) return true;
+        if(p==null || q==null) return false;
+
+        Queue<TreeNode> q1 = new LinkedList<>();
+        Queue<TreeNode> q2 = new LinkedList<>();
+
+        q1.add(p);
+        q2.add(q);
+
+        while(!q1.isEmpty() && !q2.isEmpty()) {
+            TreeNode ptree = q1.poll();
+            TreeNode qtree = q2.poll();
+
+            if(ptree.val != qtree.val) return false;
+
+            if(ptree.left != null && qtree.left != null) {
+                q1.add(ptree.left);
+                q2.add(qtree.left);
+            } else if(ptree.left != null || qtree.left != null) return false;
+
+            if(ptree.right != null && qtree.right != null) {
+                q1.add(ptree.right);
+                q2.add(qtree.right);
+            } else if(ptree.right != null || qtree.right != null) return false;
         }
-        if(p==null || q==null) {
-            return false;
-        }
-        
-        return (p.val==q.val && isSameTree(p.left,q.left)==true && isSameTree(p.right,q.right)==true) ? true : false;
-        
+        return true;
     }
 }
