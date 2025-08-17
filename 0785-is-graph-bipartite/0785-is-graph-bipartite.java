18c5de448cd4f64a0fcfef6b2834e3e46bcc7bf1
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2025-08-18 03:39:08 +0530

    Time: 1 ms (61.88%), Space: 45.4 MB (19.27%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -2,14 +2,33 @@ class Solution {
     public boolean isBipartite(int[][] graph) {
         int V = graph.length;
         int [] colors = new int [V];
-        Arrays.fill(colors,-1);
+        Arrays.fill(colors , -1);
 
-        for(int i=0 ; i<V ; i++) {
+        for(int i=0 ; i<V ; i++ ) {
             if(colors[i] == -1) {
-                boolean checkDfs = dfs( i , 0 , colors , graph );
-                boolean checkBfs = bfs( i , 0 , colors , graph );
+                boolean check = bfs( i , 0 , colors , graph);
+                if(!check) return false;
+            }
+        }
+        return true;
+    }
 
-                if(checkDfs == false) return false;
+    public boolean bfs(int curr , int c , int [] colors , int [][] graph) {
+       Queue<Integer> q = new LinkedList<>();
+       q.add(curr);
+       colors[curr] = c;
+
+        while(!q.isEmpty()) {
+            int currNode = q.poll();
+
+            for(int neigh : graph[currNode]) {
+                if(colors[neigh] == -1) {
+                    q.add(neigh);
+                    colors[neigh] = 1- colors[currNode];
+                }
+                else if(colors[neigh] == colors[currNode]) {
+                    return false;
+                }
             }
         }
         return true;
@@ -17,41 +36,15 @@ class Solution {
 
     public boolean dfs(int curr , int c , int [] colors , int [][] mat) {
         colors[curr] = c;
-        int size = mat[curr].length;
 
-        for(int neigh=0 ; neigh<size ; neigh++) {
-            int neighNode = mat[curr][neigh];
-            if(colors[neighNode] == -1) {
-                if(dfs(neighNode , 1-c , colors , mat) == false) {
+        for(int neigh : mat[curr]) {
+            if(colors[neigh] == -1) {
+                if(dfs(neigh , 1-c , colors , mat) == false) {
                     return false;
                 }
             }
-            else if(colors[neighNode] == colors[curr]) {
+            else if(colors[neigh] == colors[curr]) {
                 return false;
-            }
-        }
-        return true;
-    }
-
-    public boolean bfs(int curr , int c , int [] colors , int [][] mat) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(curr);
-        colors[curr] = c;
-
-        while(!q.isEmpty()) {
-            int currNode = q.poll();
-            int size = mat[currNode].length;
-
-            for(int neigh=0 ; neigh < size ; neigh++) {
-                int neighNode = mat[currNode][neigh];
-
-                if(colors[neighNode] == -1) {
-                    q.add(neighNode);
-                    colors[neighNode] = 1 - colors[currNode];
-                }
-                else if(colors[neighNode] == colors[currNode]) {
-                    return false;
-                }
             }
         }
         return true;
