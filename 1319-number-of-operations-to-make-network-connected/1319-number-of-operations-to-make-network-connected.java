6a174c0100867fccae359aab51578d588e222528
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2025-08-18 20:22:45 +0530

    Time: 20 ms (13.95%), Space: 65.8 MB (32.42%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,27 +1,48 @@
 class Solution {
     public int makeConnected(int n, int[][] connections) {
-        int len = connections.length;
-        if(n-1 > len) return -1;
-        List<List<Integer>> adj = new ArrayList<>();
-        for(int i=0 ;i<n ; i++) {
-            adj.add(new ArrayList<>());
-        }
+        if(n-1 > connections.length) return -1;
+        List<List<Integer>> adj = create(n , connections);
 
-        for(int [] e : connections) {
-            adj.get(e[0]).add(e[1]);
-            adj.get(e[1]).add(e[0]);
-        } 
-
-        int count = 0;
         boolean [] vis = new boolean [n];
-        for(int i=0 ; i<n ; i++) {
-            if(!vis[i]) {
-                dfs( i , vis , adj);
-                count++;
+        Queue<Integer> q = new LinkedList<>();
+
+        // BFS component counting
+        boolean[] visBfs = new boolean[n];
+        int componentsBfs = 0;
+        for (int i = 0; i < n; i++) {
+            if (!visBfs[i]) {
+                bfs(i, visBfs, adj);
+                componentsBfs++;
             }
         }
 
-        return count-1;
+        // DFS component counting
+        boolean[] visDfs = new boolean[n];
+        int componentsDfs = 0;
+        for (int i = 0; i < n; i++) {
+            if (!visDfs[i]) {
+                dfs(i, visDfs, adj);
+                componentsDfs++;
+            }
+        }
+
+        return componentsDfs-1;
+    }
+
+    public void bfs(int node , boolean [] vis ,  List<List<Integer>> adj) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(node);
+        vis[node] = true;
+
+        while(!q.isEmpty()) {
+            int curr = q.poll();
+            for(int neigh : adj.get(curr)) {
+                if(!vis[neigh]) {
+                    q.add(neigh);
+                    vis[neigh] = true;
+                }
+            }
+        }
     }
 
     public void dfs(int curr , boolean [] vis , List<List<Integer>> adj) {
@@ -32,5 +53,18 @@ class Solution {
                 dfs(neigh , vis , adj);
             }
         }
+    }
+
+    public List<List<Integer>> create(int n , int [][] connections) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0 ;i<n ; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for(int [] e : connections) {
+            adj.get(e[0]).add(e[1]);
+            adj.get(e[1]).add(e[0]);
+        } 
+        return adj;
     }
 }
