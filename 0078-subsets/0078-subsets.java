b6f366d396e64fd9aa0a63e77a62e7011740e2a8
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2025-08-26 23:14:04 +0530

    Time: 0 ms (100%), Space: 42.8 MB (65.64%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,21 +1,16 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-        int n = nums.length;
-        solve( 0 , n , nums , temp , ans);
-        return ans;
-    }
-
-    public void solve(int curr , int len , int [] nums , List<Integer> temp , List<List<Integer>> ans) {
-        ans.add(new ArrayList<>(temp));
-
-        for(int i=curr ; i<len ; i++) {
-            temp.add(nums[i]);
-
-            solve(i+1 , len , nums , temp , ans);
-
-            temp.remove(temp.size()-1);
+        ans.add(new ArrayList<>()); // Start with empty subset
+        
+        for (int num : nums) {
+            int size = ans.size();
+            for (int i = 0; i < size; i++) {
+                List<Integer> newSubset = new ArrayList<>(ans.get(i));
+                newSubset.add(num);
+                ans.add(newSubset);
+            }
         }
+        return ans;
     }
 }
