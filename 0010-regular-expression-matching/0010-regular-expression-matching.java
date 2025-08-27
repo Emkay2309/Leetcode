2fa8af40aeb4cd31b3adb649ecb1475854464271
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2025-08-27 23:47:06 +0530

    Time: 1 ms (100%), Space: 42.4 MB (26.58%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,14 +1,31 @@
 class Solution {
+    Boolean[][] memo;  // memo[i][j] stores result of s[i:], p[j:]
+
     public boolean isMatch(String s, String p) {
-        if (p.length() == 0) return s.length() == 0;
+        memo = new Boolean[s.length() + 1][p.length() + 1];
+        return dfs(0, 0, s, p);
+    }
 
-        boolean firstMatch = (s.length() > 0 &&  (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.') );
+    private boolean dfs(int i, int j, String s, String p) {
+        // if we already computed this state
+        if (memo[i][j] != null) return memo[i][j];
 
-        if (p.length() >= 2 && p.charAt(1) == '*') {
-            boolean skipXStar    = isMatch(s, p.substring(2));
-            boolean useXStar  =  firstMatch && isMatch(s.substring(1), p);
-            return skipXStar || useXStar;
-        }     
-        return firstMatch && isMatch(s.substring(1), p.substring(1));
+        boolean ans;
+
+        if (j == p.length()) {
+            ans = (i == s.length()); // both must end
+        } else {
+            boolean firstMatch = (i < s.length() &&
+                    (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.'));
+
+            if (j + 1 < p.length() && p.charAt(j + 1) == '*') {
+                // skip "x*" OR use "x*"
+                ans = dfs(i, j + 2, s, p) || (firstMatch && dfs(i + 1, j, s, p));
+            } else {
+                ans = firstMatch && dfs(i + 1, j + 1, s, p);
+            }
+        }
+
+        return memo[i][j] = ans;  // save result
     }
 }
