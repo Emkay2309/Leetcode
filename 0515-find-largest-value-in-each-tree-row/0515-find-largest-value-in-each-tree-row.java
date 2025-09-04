16318192df1dab2ec470ebd2dbfb5f56b0df50a7
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2025-09-04 14:38:15 +0530

    Time: 0 ms (100%), Space: 45.4 MB (69.55%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.java
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.java
@@ -14,30 +14,25 @@
  * }
  */
 class Solution {
+    List<Integer> ans;
     public List<Integer> largestValues(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        
+        ans = new ArrayList<>();
         if(root == null) return ans;
-
-        while(!q.isEmpty()) {
-            int max = Integer.MIN_VALUE;
-            int size = q.size();
-
-            for(int i=0 ; i<size; i++) {
-                TreeNode curr = q.poll();
-                max = Math.max(max , curr.val);
-
-                if(curr.left != null) {
-                    q.add(curr.left);
-                }
-                if(curr.right != null) {
-                    q.add(curr.right);
-                }
-            }
-            ans.add(max);
-        }
+        dfs(root , 0 );
         return ans;
+    }
+
+    public void dfs(TreeNode root , int level ) {
+        if(root == null) return;
+
+        if(ans.size() == level) {
+            ans.add(root.val);
+        }
+        else {
+            ans.set(level , Math.max(ans.get(level) , root.val));
+        }
+
+        dfs(root.left , level+1);
+        dfs(root.right , level+1);
     }
 }
