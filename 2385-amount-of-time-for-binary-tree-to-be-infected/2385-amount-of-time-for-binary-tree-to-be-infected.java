6ef720791e25452609d5f19ab59e70f951581c19
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2025-09-05 21:50:45 +0530

    Time: 193 ms (5.1%), Space: 139.8 MB (5.06%) - LeetHub

--- 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.java
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.java
@@ -13,31 +13,56 @@
  *     }
  * }
  */
+
+
+import java.util.*;
+
 class Solution {
-    int ans;
     public int amountOfTime(TreeNode root, int start) {
-        ans = 0;
-        dfs(root , start);
-        return ans;
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        convert(root, 0, map);
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        int minute = 0;
+        Set<Integer> visited = new HashSet<>();
+        visited.add(start);
+
+        while (!q.isEmpty()) {
+            int levelSize = q.size();
+            while (levelSize > 0) {
+                int current = q.poll();
+
+                for (int num : map.get(current)) {
+                    if (!visited.contains(num)) {
+                        visited.add(num);
+                        q.add(num);
+                    }
+                }
+                levelSize--;
+            }
+            minute++;
+        }
+        return minute - 1;
     }
 
-    public int dfs(TreeNode root , int k) {
-        if(root == null) return 0;
-
-        int left = dfs(root.left, k);
-        int right = dfs(root.right , k);
-
-        if(root.val == k) {
-            ans = Math.max(left,right);
-            return -1;
+    void convert(TreeNode current, int parent, Map<Integer, Set<Integer>> map) {
+        if (current == null) {
+            return;
+        } 
+        if (!map.containsKey(current.val)) {
+            map.put(current.val, new HashSet<>());
         }
-        else if(left >= 0  &&  right >=0) {
-            return 1 + Math.max(left,right);
+        Set<Integer> adjacentList = map.get(current.val);
+        if (parent != 0) {
+            adjacentList.add(parent);
+        } 
+        if (current.left != null) {
+            adjacentList.add(current.left.val);
+        } 
+        if (current.right != null) {
+            adjacentList.add(current.right.val);
         }
-        else {
-            int dist = Math.abs(left) + Math.abs(right);
-            ans = Math.max(ans , dist);
-            return Math.min(left , right) - 1;
-        }
+        convert(current.left, current.val, map);
+        convert(current.right, current.val, map);
     }
 }
