1810f4bb4f45f1382f6f7e850690e803350e68a7
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2025-09-17 21:33:31 +0530

    Time: 8 ms (50.67%), Space: 57 MB (60.7%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -0,0 +1,36 @@
+public class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> ans = new ArrayList<>();
+        dfs(s, 0, new ArrayList<>(), ans);
+        return ans;
+    }
+
+    private void dfs(String s, int start, List<String> path, List<List<String>> ans) {
+        // If we've reached the end of the string, add the current partition to the ans list
+        if (start == s.length()) {
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+        // Explore all possible partitions
+        for (int end = start + 1; end <= s.length(); end++) {
+            // If the current substring is a palindrome, add it to the current path
+            if (isPalindrome(s, start, end - 1)) {
+                path.add(s.substring(start, end));
+                // Recur to find other partitions
+                dfs(s, end, path, ans);
+                // Backtrack to explore other partitions
+                path.remove(path.size() - 1);
+            }
+        }
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+        // Check if the substring s[left:right+1] is a palindrome
+        while (left < right) {
+            if (s.charAt(left++) != s.charAt(right--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
