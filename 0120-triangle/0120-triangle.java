292272d14ba8f6e887ccbf116815f9914ebb0c08
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2025-09-26 13:53:18 +0530

    Time: 1 ms (99.85%), Space: 45.5 MB (42.28%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,21 +1,18 @@
 class Solution {
-
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
         Integer [][] dp = new Integer [n][n];
-        return dfs( 0 , 0 , triangle , dp);
+        return dfs(0,0,triangle , dp);
     }
 
-    public int dfs(int i, int j , List<List<Integer>> mat , Integer [][] dp) {
-        if(i == mat.size()) return 0;
+    public int dfs(int row , int col , List<List<Integer>> mat , Integer [][] dp ) {
+        if(row == mat.size()) return 0;
 
-        if(dp[i][j] != null) {
-            return dp[i][j];
-        }
+        if(dp[row][col] != null) return dp[row][col];
 
-        Integer down = dfs(i+1 , j , mat , dp);
-        Integer right = dfs(i+1 , j+1 , mat , dp);
+        int down = dfs(row+1 , col , mat , dp);
+        int dia =  dfs(row+1 , col+1 , mat , dp);
 
-        return   dp[i][j] = mat.get(i).get(j) + Math.min(down , right);
+        return dp[row][col] = mat.get(row).get(col) + Math.min(down , dia);
     }
 }
