2c9115b1391219e9c18ebe6e0fd899263fe2780c
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2025-09-26 13:04:45 +0530

    Time: 188 ms (13.79%), Space: 43.4 MB (50.58%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.java
+++ 0611-valid-triangle-number/0611-valid-triangle-number.java
@@ -1,20 +1,32 @@
 class Solution {
     public int triangleNumber(int[] nums) {
+        int ans = 0, n = nums.length;
         Arrays.sort(nums);
-        int n = nums.length;
-        int count = 0;
 
-        for (int i = n - 1; i >= 2; i--) {
-            int left = 0, right = i - 1;
-            while (left < right) {
-                if (nums[left] + nums[right] > nums[i]) {
-                    count += right - left;
-                    right--;
-                } else {
-                    left++;
-                }
+        for (int i = 0; i < n-2; i++) {
+            for (int j = i + 1; j < n-1; j++) {
+                int ab = nums[i] + nums[j];
+                int count = getCount(j + 1, n - 1, ab, nums);
+                ans += count;
             }
         }
-        return count;
+        return ans;
+    }
+
+    public int getCount(int left, int right, int k, int[] arr) {
+        int ans = left-1;
+        int start = left-1;
+
+        while(left <= right) {
+            int mid = (left+right)/2;
+            if(k > arr[mid]) {
+                ans = mid;
+                left = mid+1;
+            }
+            else {
+                right = mid-1;
+            }
+        }
+        return ans-start;
     }
 }
