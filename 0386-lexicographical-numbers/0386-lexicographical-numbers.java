a8917924926e0acb1ee9d28ff159641b134cf2c6
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2025-09-29 22:48:18 +0530

    Time: 1 ms (100%), Space: 48.2 MB (80%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.java
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.java
@@ -1,20 +1,21 @@
 class Solution {
     public List<Integer> lexicalOrder(int n) {
         List<Integer> ans = new ArrayList<>();
-        int curr = 1;
 
-        for(int i=1 ; i<=n ; i++) {
-            ans.add(curr);
-            if(curr * 10 <= n) {
-                curr *= 10;
-            }
-            else {
-                while(curr % 10 == 9 || curr >=n) {
-                    curr = curr/10;
-                }
-                curr += 1;
-            }
+        for(int i=1 ; i<=9 ; i++) {
+            solve(i , n , ans);
         }
         return ans;
+    }
+
+    public void solve(int num , int n , List<Integer> ans) {
+        if(num > n) return;
+        ans.add(num);
+
+        for(int add=0 ; add<=9 ; add++) {
+            int newNum = (num*10)+add;
+            if(newNum > n) return;
+            solve(newNum , n , ans);
+        }
     }
 }
