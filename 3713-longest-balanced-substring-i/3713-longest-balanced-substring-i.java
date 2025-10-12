bccca26e98d988fd3794fc81f92d4dc0064142cc
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2025-10-12 09:50:23 +0530

    Time: 1987 ms (0%), Space: 45.7 MB (0%) - LeetHub

--- 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.java
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.java
@@ -1,35 +1,23 @@
 class Solution {
     public int longestBalanced(String s) {
-        
         int n = s.length();
         int ans = 0;
 
-        for(int i=0 ; i<n ; i++) {
-            int [] freq = new int [26];
-            for(int j=i ; j<n ; j++) {
-                freq[s.charAt(j)-'a']++;
+        for (int i = 0; i < n; i++) {
+            Map<Character, Integer> freq = new HashMap<>();
+            for (int j = i; j < n; j++) {
+                char curr = s.charAt(j);
+                freq.put(curr, freq.getOrDefault(curr, 0) + 1);
 
-                if(valid(freq)) {
-                    ans = Math.max(ans , j-i+1);
+                // collect counts of letters
+                Set<Integer> counts = new HashSet<>(freq.values());
+
+                if (counts.size() == 1) {
+                    ans = Math.max(ans, j - i + 1);
                 }
             }
         }
+
         return ans;
-    }
-
-    public boolean valid(int [] freq) {
-        int count = 0;
-
-        for(int f : freq) {
-            if(f>0) {
-                if(count==0) {
-                    count = f;
-                }
-                else if(count != f) {
-                    return false;
-                }
-            }
-        }
-        return true;
     }
 }
