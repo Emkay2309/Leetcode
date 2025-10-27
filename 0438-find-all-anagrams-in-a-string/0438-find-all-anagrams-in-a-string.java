5435cc5dd7ffd05ef5c6a0c6e80e47ed513cece1
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2025-10-27 17:21:43 +0530

    Time: 661 ms (11.85%), Space: 46 MB (9.59%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,40 +1,45 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> ans = new ArrayList<>();
-
         int n = s.length();
         int m = p.length();
-
-        if(n < m) return ans;
-
-        int [] sfreq = new int [26];
-        int [] pfreq = new int [26];
-
-        for(int i=0 ; i<m ; i++) {
-            char ch = p.charAt(i);
-            pfreq[ch-'a']++;
+        
+        // Fix 1: Check if s is shorter than p
+        if (n < m) {
+            return ans;
         }
 
-        for(int i=0; i<m ; i++) {
-            char ch = s.charAt(i);
-            sfreq[ch-'a']++;
-        }
-
-        for(int i=m ; i<n ; i++) {
-            if(Arrays.equals(sfreq,pfreq)) {
-                ans.add(i-m);
+        for (int i = 0; i <= n - m; i++) {
+            String s1 = s.substring(i, i + m);
+            if (isAnagram(s1, p)) {
+                ans.add(i);
             }
-
-            char add = s.charAt(i);
-            char remove = s.charAt(i-m);
-
-            sfreq[add-'a']++;
-            sfreq[remove-'a']--;
-        }
-
-        if(Arrays.equals(sfreq,pfreq)) {
-            ans.add(n-m);
         }
         return ans;
+    }
+
+    public boolean isAnagram(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+
+        if(n != m) {
+            return false;
+        }
+
+        int [] sFreq = new int [26];
+        //int [] tFreq = new int [26];
+
+        for(int i=0 ; i < n ; i++) {
+            char sch = s.charAt(i);
+            char tch = t.charAt(i);
+
+            sFreq[sch - 'a']++;
+            sFreq[tch - 'a']--;
+        }
+
+        for(int a : sFreq) {
+            if(a != 0) return false;
+        }
+        return true;
     }
 }
