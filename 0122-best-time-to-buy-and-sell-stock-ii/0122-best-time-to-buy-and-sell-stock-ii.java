44bac04d864f6d3b877557507489fadca5b3b8eb
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2025-12-09 22:49:13 +0530

    Time: 0 ms (100%), Space: 45.6 MB (48.27%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,36 +1,18 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int n = prices.length;
+        if (prices == null || prices.length <= 1) {
+            return 0;
+        }
         int ans = 0;
-        int [][] dp = new int [n][2];
-        for(int [] a : dp) {
-            Arrays.fill(a , -1);
+        for (int i = 1; i < prices.length; i++) {
+            int curr = prices[i];
+            int prev = prices[i-1];
+
+            if (prev < curr) {
+                int profit = curr - prev;
+                ans += profit;
+            }
         }
-        int buy = 0;
-
-        return dfs(0 , n , buy , prices , dp);
-    }
-    public int dfs(int i , int n , int buy , int [] arr , int [][] dp) {
-        if(i >= n ) return 0;
-
-        if(dp[i][buy] != -1) return dp[i][buy];
-
-        int profit = 0;
-
-        //if you can buy acc to flag
-        if(buy == 0) {
-            int currBuy = -arr[i] + dfs(i+1,n,1,arr,dp);
-            int notBuy = dfs(i+1 , n , 0 , arr , dp);
-
-            profit = Math.max(currBuy , notBuy);
-        }
-        //acc to flag you have to sell
-        else {
-            int sell = arr[i] + dfs(i+1 , n , 0 , arr , dp);
-            int notSell = dfs(i+1 , n , 1 , arr , dp);
-
-            profit = Math.max(sell , notSell);
-        }
-        return dp[i][buy] = profit;
+        return ans;
     }
 }
