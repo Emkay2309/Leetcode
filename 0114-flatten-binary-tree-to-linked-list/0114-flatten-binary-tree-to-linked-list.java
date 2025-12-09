d970bbab3dd08fe7d942eb6256bd4c904b8ab79b
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2025-12-09 22:46:44 +0530

    Time: 0 ms (100%), Space: 42.3 MB (76.93%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -14,17 +14,28 @@
  * }
  */
 class Solution {
-    private TreeNode prev = null;
-    public void flatten(TreeNode root) {
-        if(root == null) return;
+        
+        // Initialize a global variable "prev" as null to keep track of the previous node in the flattened tree
+    public TreeNode prev = null;
 
+    // A method to flatten the given binary tree into a singly linked list in-place
+    public void flatten(TreeNode root) {
+        // If the root node is null, return
+        if(root == null){
+            return;
+        }
+
+        // Recursively flatten the right subtree first
         flatten(root.right);
+        // Recursively flatten the left subtree second
         flatten(root.left);
 
+        // Set the right child of the current node to the previous node in the flattened tree
         root.right = prev;
+        // Set the left child of the current node to null
         root.left = null;
-
+        // Update the previous node to be the current node
         prev = root;
-
     }
+        
 }
