86d7e3a123563f7b4b49fbaff91f2a31d9f6067d
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2025-12-18 15:31:44 +0530

    Time: 279 ms (7.99%), Space: 278.8 MB (5.21%) - LeetHub

--- 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.java
+++ 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.java
@@ -1,46 +1,42 @@
 class Solution {
-
-    private int[] prices;
-    private long[][][] memo;
-
+    Long[][][] dp;
+    
     public long maximumProfit(int[] prices, int k) {
-        this.prices = prices;
         int n = prices.length;
-        memo = new long[n][k + 1][3];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= k; j++) {
-                for (int s = 0; s < 3; s++) {
-                    memo[i][j][s] = -1;
-                }
-            }
-        }
-        return dfs(n - 1, k, 0);
+        dp = new Long[n][k + 1][3];
+        return dfs(0, 0, k, prices);
     }
 
-    private long dfs(int i, int j, int state) {
-        if (j == 0) {
-            return 0;
-        }
-        if (i == 0) {
-            return state == 0 ? 0 : (state == 1 ? -prices[0] : prices[0]);
-        }
-        if (memo[i][j][state] != -1) {
-            return memo[i][j][state];
+    public long dfs(int index, int state, int transactions, int[] prices) {
+        if (index == prices.length) {
+            if (state == 0) return 0;
+            return Integer.MIN_VALUE; // unfinished transaction is invalid
         }
 
-        int p = prices[i];
-        long res;
-        if (state == 0) {
-            res = Math.max(
-                dfs(i - 1, j, 0),
-                Math.max(dfs(i - 1, j, 1) + p, dfs(i - 1, j, 2) - p)
-            );
-        } else if (state == 1) {
-            res = Math.max(dfs(i - 1, j, 1), dfs(i - 1, j - 1, 0) - p);
-        } else {
-            res = Math.max(dfs(i - 1, j, 2), dfs(i - 1, j - 1, 0) + p);
+        if (dp[index][transactions][state] != null) {
+            return dp[index][transactions][state];
         }
-        memo[i][j][state] = res;
-        return res;
+
+        long notTake = dfs(index + 1, state, transactions, prices);
+        long take = Integer.MIN_VALUE;
+
+        if (transactions > 0) {
+            if (state == 1) {
+                // Sell today (complete normal transaction)
+                take = prices[index] + dfs(index + 1, 0, transactions - 1, prices);
+            } 
+            else if (state == 2) {
+                // Buy back today (complete short transaction)
+                take = -prices[index] + dfs(index + 1, 0, transactions - 1, prices);
+            }
+            else { 
+                // Start a new transaction
+                long buy       = -prices[index] + dfs(index + 1, 1, transactions, prices); // normal buy
+                long shortSell = prices[index] + dfs(index + 1, 2, transactions, prices); // short sell
+                take = Math.max(buy, shortSell);
+            }
+        }
+
+        return dp[index][transactions][state] = Math.max(notTake, take);
     }
 }
