0c280a571875cf731ca5fd62070184303b73a95e
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2025-12-27 20:09:02 +0530

    Time: 26 ms (9.57%), Space: 45.9 MB (6.85%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,22 +1,10 @@
 class Solution {
     public int arrangeCoins(int n) {
-        return find(0, 2147483647, n);
+        return dfs(n, 1, 0);
     }
-
-    public int find(int lo, int hi, int target) {
-        int ans = 0;
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            long sum = ((long) mid * (mid + 1)) / 2;
-            if (sum <= target) {
-                ans = mid;
-                lo = mid + 1;
-            }
-            else {
-                hi = mid - 1;
-            }
-        }
-        return (int) ans;
+    public int dfs(int blocksLeft, int blocksNeededForThisStep, int stepsBuilt) {
+        if (blocksLeft < blocksNeededForThisStep) return stepsBuilt;
+        
+        return dfs(blocksLeft - blocksNeededForThisStep, blocksNeededForThisStep + 1, stepsBuilt + 1);
     }
-
 }
