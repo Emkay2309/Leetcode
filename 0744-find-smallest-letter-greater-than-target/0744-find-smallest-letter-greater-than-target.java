cf7c438349558480175c52d199bb7b12459b5021
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2026-01-31 14:59:23 +0530

    Time: 6 ms (0.46%), Space: 46.7 MB (9.86%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.java
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.java
@@ -1,10 +1,11 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
-        for (char letter : letters) {
-            if (letter > target) {
-                return letter;
-            }
-        }
-        return letters[0];
+        return (char) IntStream.range(0, letters.length)
+                .map(i -> letters[i])  
+                .filter(c -> c > target)
+                .findFirst()
+                .orElse((int) letters[0]); 
     }
 }
