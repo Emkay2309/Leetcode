f98c888ed89cb3a38e82e80e28922d75c381cbf6
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2026-04-30 12:36:39 +0530

    Time: 368 ms (30.11%), Space: 273.6 MB (7.53%) - LeetHub

--- 3742-maximum-path-score-in-a-grid/3742-maximum-path-score-in-a-grid.java
+++ 3742-maximum-path-score-in-a-grid/3742-maximum-path-score-in-a-grid.java
@@ -2,41 +2,37 @@ class Solution {
     private int[][] grid;
     private Integer[][][] memo;
     private static final int INF = 1 << 30;
-    
+
     public int maxPathScore(int[][] grid, int k) {
         this.grid = grid;
         int m = grid.length;
         int n = grid[0].length;
         this.memo = new Integer[m][n][k + 1];
-        
+
         int ans = dfs(0, 0, k);
-        return ans < 0 ? -1 : ans;
+        return ans == -INF ? -1 : ans;
     }
-    
-    // Max score to reach (i,j) from (0,0) with exactly 'used' changes so far
-    private int dfs(int i, int j, int used) {
-        if (used > k) return -INF;
-        
-        if (i == 0 && j == 0) {
-            return grid[0][0];
+
+    private int dfs(int i, int j, int remK) {
+        if (i >= grid.length || j >= grid[0].length) return -INF;
+
+        int cost = (grid[i][j] != 0) ? 1 : 0;
+        if (remK < cost) return -INF;
+
+        // destination
+        if (i == grid.length - 1 && j == grid[0].length - 1) {
+            return grid[i][j];
         }
-        
-        if (memo[i][j][used] != null) return memo[i][j][used];
-        
-        int cost = (grid[i][j] == 0 ? 0 : 1);
-        
-        int maxScore = -INF;
-        // From up
-        if (i > 0) {
-            int prev = dfs(i - 1, j, used - cost);
-            if (prev != -INF) maxScore = Math.max(maxScore, prev + grid[i][j]);
-        }
-        // From left
-        if (j > 0) {
-            int prev = dfs(i, j - 1, used - cost);
-            if (prev != -INF) maxScore = Math.max(maxScore, prev + grid[i][j]);
-        }
-        
-        return memo[i][j][used] = maxScore;
+
+        if (memo[i][j][remK] != null) return memo[i][j][remK];
+
+        int right = dfs(i, j + 1, remK - cost);
+        int down = dfs(i + 1, j, remK - cost);
+
+        int best = Math.max(right, down);
+
+        if (best == -INF) return memo[i][j][remK] = -INF;
+
+        return memo[i][j][remK] = grid[i][j] + best;
     }
 }
