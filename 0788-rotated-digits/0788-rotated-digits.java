ef043cf94f98c621824a6ea1a8e2b000702c5df7
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2026-05-02 09:24:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.java
+++ 0788-rotated-digits/0788-rotated-digits.java
@@ -1,32 +1,33 @@
 class Solution {
     public int rotatedDigits(int n) {
+        int[] dp = new int[n + 1];
         int count = 0;
-        
-        for (int i = 1; i <= n; i++) {
-            int num = i;
-            boolean isValid = true;
-            boolean hasChange = false;
-            
-            while (num > 0) {
-                int digit = num % 10;
-                
-                if (digit == 3 || digit == 4 || digit == 7) {
-                    isValid = false;
-                    break;
+
+        for (int i = 0; i <= n; i++) {
+            if (i < 10) {
+                if (i == 0 || i == 1 || i == 8) {
+                    dp[i] = 1;
+                } else if (i == 2 || i == 5 || i == 6 || i == 9) {
+                    dp[i] = 2;
+                    count++;
+                } else {
+                    dp[i] = 0;
                 }
-                
-                if (digit == 2 || digit == 5 || digit == 6 || digit == 9) {
-                    hasChange = true;
+            } else {
+                int a = dp[i / 10];
+                int b = dp[i % 10];
+
+                if (a == 1 && b == 1) {
+                    dp[i] = 1;
+                } else if (a >= 1 && b >= 1) {
+                    dp[i] = 2;
+                    count++;
+                } else {
+                    dp[i] = 0;
                 }
-                
-                num /= 10;
-            }
-            
-            if (isValid && hasChange) {
-                count++;
             }
         }
-        
+
         return count;
     }
 }
