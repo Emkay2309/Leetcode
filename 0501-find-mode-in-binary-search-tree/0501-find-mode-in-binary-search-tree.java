e8babb6921642be6e97fa79573e60b0253b7d669
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2026-05-09 06:14:08 +0530

    Time: 89 ms (5.75%), Space: 46.8 MB (62.22%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.java
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.java
@@ -1,50 +1,40 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
+public class Solution {
+    private int currVal;
+    private int currCount = 0;
+    private int maxCount = 0;
+    private int modeCount = 0;
+    
+    private int[] modes;
+
     public int[] findMode(TreeNode root) {
-        Map<Integer, Integer> counter = new HashMap();
-        dfs(root, counter);
-        int maxFreq = 0;
-
-        for (int key : counter.keySet()) {
-            maxFreq = Math.max(maxFreq, counter.get(key));
-        }
-
-        List<Integer> ans = new ArrayList();
-        for (int key : counter.keySet()) {
-            if (counter.get(key) == maxFreq) {
-                ans.add(key);
-            }
-        }
-        
-        int[] result = new int[ans.size()];
-        for (int i = 0; i < ans.size(); i++) {
-            result[i] = ans.get(i);
-        }
-        
-        return result;
+        inorder(root);
+        modes = new int[modeCount];
+        modeCount = 0;
+        currCount = 0;
+        inorder(root);
+        return modes;
     }
 
-    public void dfs(TreeNode node, Map<Integer, Integer> counter) {
-        if (node == null) {
-            return;
+    private void handleValue(int val) {
+        if (val != currVal) {
+            currVal = val;
+            currCount = 0;
         }
-        
-        counter.put(node.val, counter.getOrDefault(node.val, 0) + 1);
-        dfs(node.left, counter);
-        dfs(node.right, counter);
+        currCount++;
+        if (currCount > maxCount) {
+            maxCount = currCount;
+            modeCount = 1;
+        } else if (currCount == maxCount) {
+            if (modes != null)
+                modes[modeCount] = currVal;
+            modeCount++;
+        }
+    }
+    
+    private void inorder(TreeNode root) {
+        if (root == null) return;
+        inorder(root.left);
+        handleValue(root.val);
+        inorder(root.right);
     }
 }
