ed29b44c514abfbe61768875bbd98eb3ddb05992
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2026-05-19 13:17:29 +0530

    Time: 7 ms (26.7%), Space: 80.3 MB (79.53%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -1,17 +1,36 @@
-class Solution {
+public class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        int n = nums1.length, m = nums2.length;
-        int i = 0, j = 0;
-
-        while (i < n && j < m) {
-            if (nums1[i] == nums2[j])
-                return nums1[i];
-
-            if (nums1[i] < nums2[j])
-                i++;
-            else
-                j++;
+        // Binary search should be done on the larger array
+        // If nums1 is longer, call getCommon with the arrays swapped
+        if (nums1.length > nums2.length) {
+            return getCommon(nums2, nums1);
         }
+        
+        // Search for each element of nums1 in nums2
+        // Return the first common element found
+        for (int num : nums1) {
+            if (binarySearch(num, nums2)) {
+                return num;
+            }
+        }
+
+        // Return -1 if there are no common elements
         return -1;
+    }
+
+    private boolean binarySearch(int target, int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
     }
 }
