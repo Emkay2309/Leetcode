24a9c387baba5eb65c58270130e67a26d627ede2
Md Mohsin <77264192+Emkay2309@users.noreply.github.com>
2026-08-16 15:19:37 +0530

    Time: 10 ms (12.88%), Space: 49.8 MB (5.01%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,18 +1,19 @@
 class Solution {
+    Integer [][] dp;
+    int n;
     public boolean stoneGame(int[] piles) {
-        int n = piles.length;
-        Integer[][] memo = new Integer[n][n];
-        return solve(0, n - 1, piles, memo) > 0;
+        n = piles.length;
+        dp = new Integer [n][n];
+        return solve(0,n-1 , piles) > 0;
     }
-    
-    private int solve(int l, int r, int[] piles, Integer[][] memo) {
-        if (l > r) return 0;
-        if (memo[l][r] != null) return memo[l][r];
-        
-        int takeLeft = piles[l] - solve(l + 1, r, piles, memo);
-        int takeRight = piles[r] - solve(l, r - 1, piles, memo);
-        
-        memo[l][r] = Math.max(takeLeft, takeRight);
-        return memo[l][r];
+
+    public int solve(int left , int right , int [] piles) {
+        if(left > right) return 0;
+        if(dp[left][right] != null) return dp[left][right];
+
+        int takeLeft  = piles[left]  - solve(left+1 , right , piles);
+        int takeRight = piles[right] - solve(left , right-1 , piles);
+
+        return dp[left][right] = Math.max(takeLeft , takeRight); 
     }
 }
